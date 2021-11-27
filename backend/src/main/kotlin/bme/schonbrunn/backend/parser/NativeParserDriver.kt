0a3e6d4068de523c9f55202fa2d91522043153c6
Tamás Nagy (T-bond) <tbondvagyok@gmail.com>
2021-11-27 19:14:41 +0100

    Add more platform independent code

--- backend/src/main/kotlin/bme/schonbrunn/backend/parser/NativeParserDriver.kt
+++ backend/src/main/kotlin/bme/schonbrunn/backend/parser/NativeParserDriver.kt
@@ -3,10 +3,26 @@ package bme.schonbrunn.backend.parser
 object NativeParserDriver {
 
     init {
-        System.load(System.getProperty("user.dir") + "/libparser.so")
+        System.load(System.getProperty("user.dir") + "/" + libName())
     }
 
     external fun parse(file: String): CAFF?
     external fun preview(input: String, output: String): Boolean
+
+    private fun libName(): String {
+        val os = System.getProperty("os.name").lowercase()
+        return when {
+            os.contains("win") -> {
+                "parser.dll"
+            }
+            os.contains("nix") || os.contains("nux") || os.contains("aix") -> {
+                "libparser.so"
+            }
+            os.contains("mac") -> {
+                "libparser.dylib"
+            }
+            else -> "libparser"
+        }
+    }
 
 }

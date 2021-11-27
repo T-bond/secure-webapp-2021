96ee6db0ebf0db587c77abb1ab45d1848d8c80ac
Tamás Nagy (T-bond) <tbondvagyok@gmail.com>
2021-11-27 03:16:30 +0100

    Add missed Argon2i implementation

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,6 +25,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.69")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
